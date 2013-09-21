45c7f9bea79d7f25bb50ed492f300a9f44a43909
Stuart Moncrieff <stuart@myloadtest.com>
2013-09-22 09:25:55 +1000

    Instructions for copying script to /www/vugen/

---
+++ www/vugen/copy_only_one_vugen_script_to_this_folder.php
@@ -0,0 +1,10 @@
+<?php
+
+/*
+ * For the VuGen Local Replay tool to work, you must copy a VuGen script to 
+ * the /www/vugen folder. The VuGen script must be a web-based script with 
+ * a recording log and recording snapshots. If you copy more than one script
+ * to the folder, then the program will throw an error.
+ */
+
+?>
